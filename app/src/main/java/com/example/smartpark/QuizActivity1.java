495e28d3876ba4c0f5f0947f2d49a87f7c4cfc33
kanado5385-k <81556800+kanado5385-k@users.noreply.github.com>
2023-09-08 14:09:04 +0900

    test

--- app/src/main/java/com/example/smartpark/QuizActivity1.java
+++ app/src/main/java/com/example/smartpark/QuizActivity1.java
@@ -10,5 +10,6 @@ public class QuizActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz1);
+        //test
     }
 }
